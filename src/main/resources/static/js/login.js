610d2a6d80dcc3d5347dc1dd581cea61ca4a3f2e
kyeongseongkang <wh___holic@naver.com>
2021-11-26 14:48:36 +0900

    로그인 기능 추가

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,50 @@
+const form = document.querySelector("#loginForm");
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw");
+
+function doSubmit(event){
+    event.preventDefault();
+    submitAjax();
+}
+
+const submitAjax = () => {
+	const param = {"id":id.value, "pw":pw.value};
+	
+	$.ajax({
+		url: "loginDo",
+        type: "POST",
+        data: param
+	}).done(function(data) {
+		console.log(data);
+		let dataId = "";
+		let dataPw = "";
+		
+		if(data.length === 0) {
+			dataId = "";
+			dataPw = "";
+		} else if(data.length > 0) {
+			for(var i=0; i<data.length; i++) {
+				dataId = data[0].id
+				dataPw = data[0].pw
+			}
+		}
+		
+		if (dataId === id.value && dataPw === pw.value) {
+			alert("로그인 성공");
+			form.id = id.value;
+			form.action = "index";
+			form.method = "POST";
+			form.submit();
+		} else if(dataId === id.value && dataPw !== pw.value) {
+			alert("비밀번호가 틀렸습니다");
+		} else {
+			alert("아이디가 틀렸습니다");
+		} 
+	}).fail(function() {
+			alert("오류가 발생하였습니다");
+			history.back();
+		}
+	);
+}
+
+form.addEventListener("submit", doSubmit);
