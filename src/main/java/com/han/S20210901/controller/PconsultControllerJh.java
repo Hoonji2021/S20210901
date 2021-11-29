ea5bea6f3dddd3ae8976448aace076481502d95f
black-sheepwall <mrloveletter@naver.com>
2021-11-29 10:46:05 +0900

    2021.11.29. morning

--- src/main/java/com/han/S20210901/controller/PconsultControllerJh.java
+++ src/main/java/com/han/S20210901/controller/PconsultControllerJh.java
@@ -25,31 +25,12 @@ public class PconsultControllerJh {
 	@Autowired
 	private ReplysService replysService;
 
-<<<<<<< HEAD
+
 	 
 	
-//	@RequestMapping(value = "index")
-//	public String test1(Model model) {
-//		String id = "a1";
-//		model.addAttribute("id", id);
-//		return "main";
-//	}
-	
 
 
-=======
->>>>>>> f18bdefd1f780f1c73da5c053616156656604972
-	@RequestMapping(value = "index")
-	public String test1(Model model) {
-		String id = "a1";
-		model.addAttribute("id", id);
-		return "main";
-	}
 
-<<<<<<< HEAD
-
-=======
->>>>>>> f18bdefd1f780f1c73da5c053616156656604972
 	@RequestMapping("pConsultCount")
 	public String pConsultList(String currentPage, String id, Model model, Pconsult pconsult) {
 		System.out.println("PcontrollerJh pConsultList() start...");
@@ -60,17 +41,14 @@ public class PconsultControllerJh {
 		
 		//페이징 totalCnt = 12,1
 		Paging pg = new Paging(totalCnt, currentPage);
-<<<<<<< HEAD
+
 		
 		pconsult.setStart(pg.getStart());
 		pconsult.setEnd(pg.getEnd());
 		
-=======
+
 		
-		pconsult.setStart(pg.getStart());
-		pconsult.setEnd(pg.getEnd());
-		
->>>>>>> f18bdefd1f780f1c73da5c053616156656604972
+
 		// 리스트 모두 가져오기
 		List<Pconsult> pConsultList = pconsultService.pConsultAll(pconsult);
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -7,13 +7,8 @@
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
-
 <body>
-<<<<<<< HEAD
-git test
-=======
-merge test
->>>>>>> 0f8ad733d6930d74fedf94b11c97d5fbcdf52eea
+
 <%
 	response.sendRedirect("main");
 %>
